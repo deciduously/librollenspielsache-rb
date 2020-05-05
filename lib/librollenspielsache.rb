461121f000387e720839045506dd9fa5e4118b2e
Ben Lovy <ben@deciduously.com>
2020-05-05 15:29:29 -0400

    Initial commit

---
+++ lib/librollenspielsache.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'ffi'
+require 'librollenspielsache/version'
+
+module Librollenspielsache
+  class Error < StandardError; end
+end
---
+++ spec/librollenspielsache/rb_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+RSpec.describe Librollenspielsache do
+  it 'has a version number' do
+    expect(Librollenspielsache::VERSION).not_to be nil
+  end
+
+  it 'Obtains a default roll from a string' do
+    # TODO
+    expect(true).to eq(true)
+  end
+end
