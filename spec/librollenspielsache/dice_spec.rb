55f5ba5d7b2c1acc20e8b974ff1b1f26580ceef2
Ben Lovy <ben@deciduously.com>
2020-05-07 18:31:00 -0400

    RollResult#to_json

--- spec/librollenspielsache/dice_spec.rb
+++ spec/librollenspielsache/dice_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'json'
+
 TEST_REPS = 200
 
 RSpec.describe Rollenspielsache::Dice do
@@ -34,6 +36,17 @@ RSpec.describe Rollenspielsache::Dice do
       total = roll.execute.total
       expect(total).to be >= 1
       expect(total).to be <= 6 * 20 - 10
+    end
+  end
+
+  it 'produces JSON from a roll result' do
+    roll = Rollenspielsache::Dice::Roll.from_str('1d2+5')
+    TEST_REPS.times do
+      result = roll.execute
+      json = result.to_json
+      option_one = { base: 1, offset: 5 }.to_json
+      option_two = { base: 2, offset: 5 }.to_json
+      expect(json).to eq(option_one) || eq(option_two)
     end
   end
 end
